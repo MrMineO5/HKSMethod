1c2f7b596ca27fd53d713392ca8135c3141770ff
MrMineO5 <petzmagnus@gmail.com>
2025-04-24 11:20:30 +0200

    Create stability check example

---
+++ examples/stability.rs
@@ -0,0 +1,55 @@
+use std::{env, thread};
+use master_research_project::models::main_model::MainModel;
+use master_research_project::scanner::consumer::stability_consumer::StabilityConsumer;
+use master_research_project::scanner::multi_threaded_scanner::MultiThreadedScanner;
+use master_research_project::simulation;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Setting up...");
+    let coupling_ranges = [
+        (0.425, 0.425),
+        (-1., 1.),
+        (-1., 1.),
+        (-1., 1.),
+        (-1., 1.),
+        (-1., 1.),
+        (-1., 1.),
+    ];
+
+    let num_samples = args[1].parse::<u64>().unwrap();
+    // let num_samples = 10000;
+    let num_threads = thread::available_parallelism()
+        .expect("Failed to get available parallelism")
+        .get();
+
+    let consumer: StabilityConsumer<7, 400, 400> = StabilityConsumer::new(coupling_ranges);
+
+    let mut scanner = MultiThreadedScanner::new(
+        coupling_ranges,
+        simulation::IntegrationParameters {
+            initial_scale: 1.22E19_f64.ln(),
+            final_scale: 1.0E11_f64.ln(),
+            num_steps: 1000000,
+        },
+        MainModel,
+        consumer
+    );
+
+    scanner.scan(num_threads, num_samples);
+
+    let images = scanner.consumer.render();
+
+    // Create out directory
+    std::fs::create_dir_all("out").expect("Failed to create output directory");
+
+    for i in 0..images.len() {
+        for j in 0..images[i].len() {
+            let filename = format!("out/stability_{}_{}.png", i, j);
+            images[i][j].save_to_png(&filename).unwrap();
+        }
+    }
+
+    println!("Done!");
+}
